10baa460229c6181792cf9394b5c4e252b4f81c3
Wilson Prado <wilson.prado@superlogica.com>
2019-05-13 11:05:05 -0300

    Add a conf file

--- public/index.php
+++ public/index.php
@@ -12,6 +12,7 @@
 */
 
 $app = require __DIR__.'/../bootstrap/app.php';
+require_once __DIR__.'../app/Helpers/conf.php';
 
 /*
 |--------------------------------------------------------------------------
